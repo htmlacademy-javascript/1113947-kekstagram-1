63bbe1f7a333eca75e9827fe0c3fa86c38b746aa
Nasiba Dzh <88klavish19@gmail.com>
2024-03-12 18:40:26 +0600

    удалился комментарий

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,3 @@
-//рандомит число
 const getRandomInteger = function (min, max) {
   const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
   const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
